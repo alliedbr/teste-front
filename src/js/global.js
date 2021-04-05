ecb3bedf8c6779414afd1fe506b347e2fe989d45
Bruno Dulcetti <brunodulcetti@gmail.com>
2021-04-05 15:46:11 -0300

    refactor: change name to allied brasil and color

--- src/js/global.js
+++ src/js/global.js
@@ -5,7 +5,7 @@ window.onload = function () {
     msg.innerHTML = 'Vagas encerradas!'
     document.getElementById('alert').appendChild(msg);
   } else {
-    console.log('%cWooza', 'color: #c4d82d; font-size: 24px;');
+    console.log('%cAllied Brasil', 'color: #003b71; font-size: 24px;');
     console.log('Primeira dica:');
     console.log('TyBpbXBvcnRhbnRlIMOpIHRlciBlc3RpbG8uIE91IG7Do28gdGVyLCBlbSBhbGd1bnMgY2Fzb3MgOyk=');
   }
